ad414b357fe6fbccd95c9e014f51fd6f62e7f3c1
Vinicius Maia <vinicius.maia@compasso.com.br>
2021-11-27 11:55:46 -0300

    first commit

---
+++ src/test/java/br/tec/vcm/learnspringboot/LearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.vcm.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
